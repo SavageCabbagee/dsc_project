f46d2f70d845fbbdf0681671ab13bcd1220004ca
SavageCabbagee <92659267+SavageCabbagee@users.noreply.github.com>
2024-12-02 16:38:10 +0800

    fix heartbeat behavior

--- backend/raft/leader.go
+++ backend/raft/leader.go
@@ -3,9 +3,27 @@ package raft
 import "fmt"
 
 func (node *RaftNode) SendHeartbeat(peerId int32, followerState *FollowerState) {
-	req := &AppendEntriesArgs{node.stableState.GetCurrentTerm(), node.id, followerState.nextIndex - 1, node.logStore.GetLog(followerState.nextIndex - 1).Term, node.logStore.logs, node.commitIndex}
-	// do not care about the reply
-	node.SendAppendEntries(peerId, req)
+	success := false
+	for !success {
+		req := &AppendEntriesArgs{node.stableState.GetCurrentTerm(), node.id, followerState.nextIndex - 1, node.logStore.GetLog(followerState.nextIndex - 1).Term, node.logStore.logs, node.commitIndex}
+
+		res, err := node.SendAppendEntries(peerId, req)
+		if err != nil {
+			fmt.Printf("ERROR %v", err)
+			return
+		}
+		if res.Term > node.stableState.GetCurrentTerm() {
+			node.updateTerm(res.Term)
+			return
+		}
+		if !res.Success {
+			followerState.nextIndex--
+			continue
+		}
+		success = true
+	}
+	followerState.nextIndex = node.logStore.GetLastLogIndex() + 1
+	followerState.matchIndex = node.logStore.GetLastLogIndex()
 }
 
 type FollowerState struct {
